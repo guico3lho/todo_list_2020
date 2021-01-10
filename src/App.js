cb7d7299511a3087459d2a16e0e71aae7f10c25d
GuiCoelho <guiminervino@gmail.com>
2021-01-10 08:55:29 -0300

    feat: Adiciona criação de tarefas
    Permite escrever tarefas e dar submit nelas.
    Após submit, é adicionado na task list
    Falta:
    Marcar item como feito, remover item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from "react";
+import Styles from "./styles.css"
 function App() {
+  const [task, setTask] = useState();
+  const [tasks, setTasks] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+
+    // FIXME: Pq são diferentes seus resultados?
+    // console.log(task);
+    // console.log(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTasks([...tasks, {
+      id: tasks.length,
+      value: task
+    }])
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+
+        <input type="text" onChange={handleChange}>
+          {/* TODO: write the task */}
+
+        </input>
+        <button type="submit">
+          {/* TODO: when pressed, add task to the task list */}
+        Adicionar
+      </button>
+      </form>
+      <ul>
+        {/* TODO: Map para renderizar as tasks */}
+        {tasks.map(task => (
+          < li key={ task.id}> {task.id} - {task.value}</li>
+        ))}
+
+      </ul>
+    </div >
   );
 }
 
