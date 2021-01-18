1904b283099b26977e041ca3295ff1879b17da98
GuiCoelho <guiminervino@gmail.com>
2021-01-18 00:48:12 -0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ function App() {
 
   const addTodo = text => {
     //Lida com a criação do componente Task (id, text, done, ..., etc)
-    const newTodo = {id: tasks.length, text: text, done: false}
+    const newTodo = { id: tasks.length, text: text, done: false }
     setTasks([...tasks, newTodo]);
   }
 
   const checkTodo = event => {
-    
-    if(event.target.checked){
+
+    if (event.target.checked) {
       return true;
-      
-    }else {
+
+    } else {
       return false;
     }
     // const newTodo = {...newTodo, done: event.target.checked}
   }
 
   const removeTodo = (index) => {
-    
+
     const beforeArray = tasks.slice(0, index);
     const afterArray = tasks.slice(index + 1);
 
@@ -34,7 +34,9 @@ function App() {
   return (
     <div>
       <Input addTodo={addTodo} />
-      <List tasks={tasks} removeTodo={removeTodo} checkTodo={checkTodo}/>
+      <List tasks={tasks}
+        removeTodo={removeTodo}
+        checkTodo={checkTodo} />
     </div >
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,21 @@
 
-import {Task} from "./Task"
-export const List = ({ tasks ,removeTodo, checkTodo}) => {
+import { Task } from "./Task"
+export const List = ({ tasks, removeTodo, checkTodo }) => {
 
-    const handleRemove = (event ) => {
-        // console.log(event.target.getAttribute("reactFiber$5qdat6zojqv"))
-        
-    }
+
     return (
         <ul>
             {/* TODO: Remove task quando concluido */}
             {
-                
+
                 tasks.map((task, index) => (
                     //Função map funciona apenas com Arrays!
-                    <Task key={task.id} index={index} task={task} removeTodo={removeTodo} checkTodo={checkTodo}/>
-                    
+                    <Task key={task.id}
+                        index={index}
+                        task={task}
+                        removeTodo={removeTodo}
+                        checkTodo={checkTodo} />
+
                 ))}
 
         </ul>
